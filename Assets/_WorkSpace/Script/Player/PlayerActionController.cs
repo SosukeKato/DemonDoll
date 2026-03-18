2a213d3c6ddc5d7c0c9ac1a1a7bf88df3fa56716
SosukeKato <dokindokinpiano@gmail.com>
2026-03-18 23:40:18 +0900

    [add]Player.csを追加

--- Assets/_WorkSpace/Script/Player/PlayerActionController.cs
+++ Assets/_WorkSpace/Script/Player/PlayerActionController.cs
@@ -1,15 +1,8 @@
 using UnityEngine;
 
-public class PlayerActionController : MonoBehaviour
+public class PlayerActionController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public void Move()
     {
         
     }
