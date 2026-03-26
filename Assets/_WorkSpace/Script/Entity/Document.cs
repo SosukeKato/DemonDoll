846bf18cc79ba640e1e29946339c19c0f461dcf2
SosukeKato <dokindokinpiano@gmail.com>
2026-03-26 18:05:52 +0900

    [update]DocumentとKeyがEntityを継承

--- Assets/_WorkSpace/Script/Entity/Document.cs
+++ Assets/_WorkSpace/Script/Entity/Document.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Document : MonoBehaviour
+public class Document : Entity
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
