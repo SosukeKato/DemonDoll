a029370ef21ca2b10f7fae7e0ed68613c66de23f
SosukeKato <dokindokinpiano@gmail.com>
2026-04-14 13:23:35 +0900

    [update]InteractされたときにIDに対応したフラグを回収する処理を作る準備

--- Assets/_WorkSpace/Script/Entity/Entity.cs
+++ Assets/_WorkSpace/Script/Entity/Entity.cs
@@ -1,18 +1,24 @@
+using System;
 using UnityEngine;
 
 public class Entity : MonoBehaviour,IInteractable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [NonSerialized] public int EntityID;
+
+    [SerializeField,Header("フラグ管理用のID(ゲーム進行に関係のないEntityの場合は-1に設定)")] int _entityID;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-    public virtual void OnInteract() { }
+    public virtual void OnInteract()
+    {
+        //フラグ管理スクリプトにIDを渡す処理を追加する
+    }
 }
